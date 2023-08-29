2dc3c12d8c8d7c55e9e7ad7087624add66498dad
kdaglas <daglach7@gmail.com>
2023-08-29 15:07:41 +0300

    add the float button

--- app/src/main/java/com/nish/recyclerview/DetailsActivity.kt
+++ app/src/main/java/com/nish/recyclerview/DetailsActivity.kt
@@ -1,9 +1,9 @@
 package com.nish.recyclerview
 
-import android.content.Intent
+//import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+//import android.widget.Button
 import android.widget.ImageView
 
 class DetailsActivity : AppCompatActivity() {
@@ -11,13 +11,13 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         val image = intent.getIntExtra("image", R.drawable.tank_tops )
-        val name = intent.getStringExtra("activityName")
+//        val name = intent.getStringExtra("activityName")
 
-        var button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener{
-            val intent1= Intent(this, InputActivity::class.java  )
-            startActivity(intent1 )
-        }
+//        var button1 = findViewById<Button>(R.id.button1)
+//        button1.setOnClickListener{
+//            val intent1= Intent(this, InputActivity::class.java  )
+//            startActivity(intent1 )
+//        }
 
 
         val icon = findViewById<ImageView>(R.id.activity_icon)
